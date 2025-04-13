2410a7a0895c739382ac95ec53df9a424fda1d33
Nikhilesh <nikkumewara2003@gmail.com>
2025-04-13 22:08:16 +0530

    hashing

--- Strivers A2Z DSA sheet/basics.py
+++ Strivers A2Z DSA sheet/basics.py
@@ -10,7 +10,7 @@ reverseTable(2)
 20 18 16 14 12 10 8 6 4 2 
 """   #output
 
-###############................basic.....math.##########
+###############              *****    basic math    *****             ##########
 """
 Input: n = 2446
 Output: 1
@@ -144,7 +144,7 @@ def armstrongNum(n):
 print(armstrongNum(9474))
 print(armstrongNum(774))
 print(armstrongNum(153))
-"""  #more sofisticated version of code. this is more tightly packed and COOL.
+"""  #more sophisticated version of code. this is more tightly packed and COOL.
 
 '''
 sum of 1 to n divisors  
@@ -199,7 +199,7 @@ def isPrime(n):
 print(isPrime(59))
 '''
 
-#############*******************   $$$$       basic recursion      $$$$   *******************######################
+########################       ****       basic recursion      *****           #####################
 
 """ 
 print up to n number using recursion
@@ -269,9 +269,9 @@ A number n is called a factorial number if it is the factorial of a positive int
 For example, the first few factorial numbers are 1, 2, 6, 24, 120,
 Given a number n, the task is to return the list/vector of the factorial numbers smaller than or equal to n.
 '''
-# at fist postion-> 1*1 =>    1      at second-> 1*2 =>    2      at third -> 2*3 =>    6       at fourth 6*4 =>24 and so on
+# at fist position-> 1*1 =>    1      at second-> 1*2 =>    2      at third -> 2*3 =>    6       at fourth 6*4 =>24 and so on
 #                 res*i => new res         new res*i    new res         new res*i    new res
-# where postion is i and previous reslt is res
+# where position is i and previous result is res
 '''
 def factorialNumbers(n):
     # code here
@@ -286,7 +286,83 @@ def factorialNumbers(n):
 print(factorialNumbers(50))
 '''
 
+'''
+reverse an array     "inplace" i.e. no extra space should be taken
+'''
+'''
+def reverse(arr):
+    left, right = 0, len(arr)-1
+    while left < right:
+        arr[left], arr[right]= arr[right], arr[left]
+        left += 1
+        right -= 1
+    return arr
+print(reverse([1,4,3,2,6,5]))
+'''
 
+'''
+valid panlindrome
+'''
+'''
+def isPalindrome(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    s = ''.join(i.lower() for i in s if i.isalnum())  # isalnum removes all non alphanumeric characters
+    newS = s[::-1]
+    if s == newS:
+        return True
+    else:
+        return False
+print(isPalindrome("A man, a plan, a canal: Panama"))
+'''
+
+'''
+fibonacci series
+'''
+'''
+def fib(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    if n == 0: return 0
+    if n == 1: return 1
+    return fib(n - 1) + fib(n - 2)
+print(fib(10))
+'''    # with recursion
+'''
+def fib(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    if n < 2: return n
+    prev1 = 1
+    prev2 = 1
+    current = prev1
+    for _ in range(2, n):
+        current = prev1 + prev2
+        prev1 = prev2
+        prev2 = current
+    return current
+print(fib(10))
+'''  # without recursion
+
+####################         *****    HASHING   *****     ###################################
+'''
+count the frequency of each digit in an arry   ---> inplace 
+'''
+
+def frequencyCount(arr):
+    hash = [0] * len(arr)
+    for i in arr:
+        if 1 <= i <= len(arr):
+            hash[i - 1] += 1
+    return hash
+
+print(frequencyCount([1,2,3,3,3,6,4]))
 
 
 
